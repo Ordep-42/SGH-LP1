1ebbaadad8e969f44dadc63b8e984e851e79e378
joao.pereira.016 <137296425+JJoaoll@users.noreply.github.com>
2024-07-14 01:47:18 +0000

    Date class Done. Please delete the ;json files.

---
+++ Date.h
@@ -0,0 +1,35 @@
+// Date.h
+#ifndef DATE_H
+#define DATE_H
+
+class Date {
+private:
+    short days;
+    short year;
+    bool isLeapYear;
+
+    void fixDate();
+
+public:
+    Date(short newDays, short newYear);
+
+    short getDay() const;
+    short getMonth() const;
+    short getYear() const;
+    short getDays() const;
+
+    
+   
+    bool isBeforeThan(Date thisDate); 
+    Date tomorrow();
+
+};
+
+// Funções auxiliares
+bool leapYearTest(short year);
+short* daysToDateBF(short day, short year);
+short dateToDaysBF(short day, short month, short year);
+
+
+
+#endif // DATE_H
