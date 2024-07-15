63c7dcfb036cdb8f5a6c7a097f81eb21eb8cad37
joao.pereira.016 <137296425+JJoaoll@users.noreply.github.com>
2024-07-15 09:59:31 -0300

    Algum progresso em criar classes (#1) by (JJoaoLL)
    * Date class Done. Please delete the ;json files.
    
    * Algum progresso em criar classes

---
+++ Patient.cpp
@@ -0,0 +1,14 @@
+// // Patient.cpp
+// #include "Patient.h"
+
+// // Construtor:
+
+// Patient::Patinet(short newDays, short newYear)
+//     : days(newDays), year(newYear), isLeapYear(leapYearTest(year)) {
+//         this->fixDate();
+//     }
+
+
+// // ------------------------------------------------------------------------------
+// // Métodos:
+// // ------------------------------------------------------------------------------
