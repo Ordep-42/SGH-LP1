4bb51ab0dacd436e67ac20378a965b6c07c8bcbc
Ordep-42 <90785456+Ordep-42@users.noreply.github.com>
2024-07-19 15:49:14 -0300

    Criação do enum Relation para o contato de emergência e ajustes na classe Patient para herança da classe User

--- include/Patient.h
+++ include/Patient.h
@@ -1,7 +1,6 @@
 // Patient.h
 #ifndef PATIENT_H
 #define PATIENT_H
-#include "Date.h"
 #include "MedicalFile.h"
 #include "User.h"
 #include <string>
@@ -9,42 +8,45 @@
 
 using namespace std;
 
+typedef enum relations {
+    Pai, Mãe, Irmão, Irmã, Avô, Avó, 
+    Tio, Tia, Sobrinho, Sobrinha,
+    Esposo, Esposa, Filho, Filha, Outro // Não consegui pensar em mais nenhum e acho q ta bom!
+} Relation;
 struct EmergencyContact {
     string name;
-    string relation;
+    Relation relation;
     string phoneNumber;
 
-    EmergencyContact(const string &newName, const string &newRelation,
+    EmergencyContact(const string &newName, Relation newRelation,
                      const string &newPhoneNumber)
         : name(newName), relation(newRelation), phoneNumber(newPhoneNumber) {}
 };
 
-struct PatientData {
-    string name;
-    const Date birthDate;
-    string sex; // vai que ne, sla...
-    string gender;
-    string civicStatus;
-
-    // vector<appointment> appointmentRegistry;
-
-    PatientData(string newName, const Date &newBirthDate, string newSex,
-                string newGender, string newCivicStatus)
-        : name(newName), birthDate(newBirthDate), sex(newSex),
-          gender(newGender), civicStatus(newCivicStatus) {}
-};
-
-class Patient {
+class Patient : public User {
   private:
-    PatientData data;
     vector<EmergencyContact> emergencyContacts;
-    User user;
     MedicalFile medicalFile;
 
     // registro de pagamentos e etc.
 
-    // Pendente
   public:
+    Patient(unsigned short newUserID, string newLogin, string newPassWord,
+            string newName, Date newBirthDate, string newCpf, Gender newGender,
+            CivilStatus newCivilStatus, string newAddress,
+            string newPhoneNumber, string newEmail, Role role,
+            vector<EmergencyContact> newEmergencyContacts,
+            MedicalFile newMedicalFile)
+        : User(newUserID, newLogin, newPassWord, newName, newBirthDate, newCpf,
+               newGender, newCivilStatus, newAddress, newPhoneNumber, newEmail),
+          emergencyContacts(newEmergencyContacts), medicalFile(newMedicalFile) {
+    }
+
+    vector<EmergencyContact> getEmergencyContacts();
+    MedicalFile getMedicalFile();
+
+    void setEmergencyContact(EmergencyContact newEmergencyContacts);
+    void setMedicalFile(MedicalFile newMedicalFile);
 };
 
 // Funções auxiliares
