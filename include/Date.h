ad7dc581cd351acc18f20b207bf23b60de686fae
joao.pereira.016 <137296425+JJoaoll@users.noreply.github.com>
2024-07-19 15:05:30 +0000

    Date update (upDate)

--- include/Date.h
+++ include/Date.h
@@ -3,28 +3,32 @@
 #define DATE_H
 
 class Date {
-  private:
-    short days;
-    short year;
-    bool isLeapYear;
-    void fixDate();
+private:
+  short days;
+  short year;
+  bool isLeapYear;
+  // void fixDate();
 
-  public:
-    Date(short newDays, short newYear);
-    Date() = default;
+public:
+  Date(short newDays, short newYear);
+  Date() = default;
 
-    short getDay() const;
-    short getMonth() const;
-    short getYear() const;
-    short getDays() const;
+  void fixDate();
 
-    bool isBeforeThan(Date thisDate);
-    Date tomorrow();
+  short getDay() const;
+  short getMonth() const;
+  short getYear() const;
+  short getDays() const;
+
+  bool isBeforeThan(Date thisDate);
+  bool isEqualTo(Date anotherDate);
+  Date tomorrow();
 };
 
 // Funções auxiliares
 bool leapYearTest(short year);
 short *daysToDateBF(short day, short year);
 short dateToDaysBF(short day, short month, short year);
+
 
 #endif // DATE_H
