b88b6fcf249755a158a5daf60f66b84a46be79aa
João Lucas de Moraes Pereira <joaoduos@gmail.com>
2024-07-31 15:02:37 +0000

    printAppointment

--- include/Time/Appointment.h
+++ include/Time/Appointment.h
@@ -37,7 +37,42 @@ class Appointment {
     void setStatus(string newStatus); // precisa conferir se é um status válido
 
     // Métodos:
-    void printAppointment() {
+    vector<string> toString(){
+      vector<string> appString;
+
+      appString[0] = "Appointment ID: " + id;
+      appString[0] += id;
+      
+      appString[1] = "Status: ";
+      appString[1] += status ;
+
+      appString[2] = "Patient ID: ";
+      appString[2] += patientID;
+
+      appString[3] = "Doctor ID: ";
+      appString[3] += doctorID;
+
+      appString[4] = "Procedure: ";
+      appString[4] += procedure;
+
+      appString[5] = "Session Date: ";
+      appString[5] += this->session.getDateString();
+
+      appString[6] = "Session Time: ";
+      appString[6] += session.getTimeString();      
+  
+    }
+
+    void printAppointment(){
+      vector<string> printMe = toString();
+      for(int i = 0; i < printMe.size(); i++){
+        cout << printMe[i] << endl;
+      }
+      
+    }
+
+    /* esse ficou 
+    void printAppointment() {  
         cout << "Appointment ID: " << id << endl;
         cout << "Status: " << status << endl;
         cout << "Patient ID: " << patientID << endl;
@@ -45,7 +80,7 @@ class Appointment {
         cout << "Procedure: " << procedure << endl;
         cout << "Session Date: " << this->session.getDateString() << endl;
         cout << "Session Time: " << session.getTimeString() << endl;
-    }
+    }  */
 };
 
 // Funções auxiliares
