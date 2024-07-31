21aa950358c57a6f9be2d5773aeb4887b1027db4
RaoniSilvestre <raoni.silvestre@hotmail.com>
2024-07-31 13:23:34 -0300

    fix: Removing unused verifications from createWSession

--- src/HospitalDatabase/WScheduleMethods/createWsession.cpp
+++ src/HospitalDatabase/WScheduleMethods/createWsession.cpp
@@ -7,32 +7,12 @@ void HospitalDatabase::createWSession(Doctor doctor, Session session) {
     string sql = "INSERT INTO WORK_SCHEDULE (DOCTOR_ID, "
                  ", SESSION) VALUES (?, ?);";
 
-    returnCode = sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr);
+    sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr);
 
-    if (!verifyErrorCode()) {
-        cout << "Preparation failed\n";
-        sqlite3_close(db);
-        return;
-    }
+    sqlite3_bind_int(stmt, 1, doctor.getUserID());
+    sqlite3_bind_text(stmt, 2, session.toString().c_str(), -1, SQLITE_STATIC);
 
-    returnCode = sqlite3_bind_int(stmt, 1, doctor.getUserID());
-    if (!verifyErrorCode()) {
-        cout << "Preparation failed\n";
-        sqlite3_finalize(stmt);
-        sqlite3_close(db);
-        return;
-    }
-
-    returnCode = sqlite3_bind_text(stmt, 2, session.toString().c_str(), -1,
-                                   SQLITE_STATIC);
-    if (!verifyErrorCode()) {
-        cout << "Preparation failed\n";
-        sqlite3_finalize(stmt);
-        sqlite3_close(db);
-        return;
-    }
-
-    returnCode = sqlite3_step(stmt);
+    sqlite3_step(stmt);
 
     sqlite3_close(db);
 }
